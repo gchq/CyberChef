fc9c2d1f6edc43d678afb2796b1a93c55d98cf3d
n1474335 <n1474335@gmail.com>
2017-03-01 21:33:28 +0000

    BUGFIX: HTML output which is then converted to a regular string (for a standard operation) still contained escaped HTML chars.

---
+++ test/tests/operations/StrUtils.js
@@ -0,0 +1,24 @@
+/**
+ * StrUtils tests.
+ *
+ * @author n1474335 [n1474335@gmail.com]
+ * @copyright Crown Copyright 2017
+ * @license Apache-2.0
+ */
+TestRegister.addTests([
+    {
+        name: "Regex, non-HTML op",
+        input: "/<>",
+        expectedOutput: "/<>",
+        recipeConfig: [
+            {
+                "op": "Regular expression",
+                "args": ["User defined", "", true, true, false, "Highlight matches"]
+            },
+            {
+                "op": "Remove whitespace",
+                "args": [true, true, true, true, true, false]
+            }
+        ],
+    },
+]);
