096891295461d6fd5e18800e702a94c1802b0a2c
David Moodie <davidmoodie12@gmail.com>
2017-06-07 19:51:09 +0100

    Only reference JPEGs in removeEXIF description to be more clear

--- src/core/config/OperationConfig.js
+++ src/core/config/OperationConfig.js
@@ -3390,9 +3390,9 @@ const OperationConfig = {
     },
     "Remove EXIF": {
         description: [
-            "Removes EXIF data from an image.",
+            "Removes EXIF data from a JPEG image.",
             "<br><br>",
-            "EXIF data is metadata embedded in images (JPEG, JPG, TIFF) and audio files.",
+            "EXIF data is metadata embedded in JPEGs.",
             "<br><br>",
             "EXIF data from photos usually contains information about the image file itself as well as the device used to create it.",
         ].join("\n"),
