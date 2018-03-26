2a2d8e9e67f8679bf97aa206793aa5e9a63f773f
Vimal-Raghubir <vraghubir0418@gmail.com>
2018-03-25 20:44:41 -0400

    Fixed formatting

--- test/tests/operations/CSVParser.js
+++ test/tests/operations/CSVParser.js
@@ -17,106 +17,106 @@ TestRegister.addTests([
         recipeConfig: [
             {
                 op: "String to CSV",
-                args: [','],
+                args: [","],
             },
         ],
     },
-	{
+    {
         name: "Testing hello world parsed to CSV",
         input: "['hello', 'world']",
         expectedOutput: "hello;world",
         recipeConfig: [
             {
                 op: "String to CSV",
-                args: [';'],
+                args: [";"],
             },
         ],
     },
-	{
+    {
         name: "Testing false parsed to CSV",
         input: false,
         expectedOutput: "The passed in data is not a string that can be converted to a CSV.",
         recipeConfig: [
             {
                 op: "String to CSV",
-                args: [','],
+                args: [","],
             },
         ],
     },
-	{
+    {
         name: "Testing ||| parsed to CSV",
         input: "|||",
         expectedOutput: "|||||",
         recipeConfig: [
             {
                 op: "String to CSV",
-                args: ['|'],
+                args: ["|"],
             },
         ],
     },
-	{
+    {
         name: "Testing 0 parsed to CSV",
         input: 0,
         expectedOutput: "The passed in data is not a string that can be converted to a CSV.",
         recipeConfig: [
             {
                 op: "String to CSV",
-                args: [','],
+                args: [","],
             },
         ],
     },
-	{
+    {
         name: "Testing 1,2,3,\n1,2, parsed to String",
         input: "1,2,3,\n,1,2,",
         expectedOutput: "[[1,2,3],[1,2]]",
         recipeConfig: [
             {
                 op: "CSV to String",
-                args: [','],
+                args: [","],
             },
         ],
     },
-	{
+    {
         name: "Testing \n\n\n parsed to String",
         input: "\n\n\n",
         expectedOutput: "[[],[],[]]",
         recipeConfig: [
             {
                 op: "CSV to String",
-                args: [','],
+                args: [","],
             },
         ],
     },
-	{
+    {
         name: "Testing 1,2,3,4,5 parsed to String",
         input: "1,2,3,4,5",
         expectedOutput: "[[1,2,3,4,5]]",
         recipeConfig: [
             {
                 op: "CSV to String",
-                args: ['|'],
+                args: ["|"],
             },
         ],
     },
-	{
+    {
         name: "Testing 0 parsed to CSV",
         input: 0,
         expectedOutput: "The passed in data is not a string that can be converted to a CSV.",
         recipeConfig: [
             {
                 op: "CSV to String",
-                args: [','],
+                args: [","],
             },
         ],
     },
-	{
+    {
         name: "Testing false parsed to CSV",
         input: false,
         expectedOutput: "The passed in data is not a string that can be converted to a CSV.",
         recipeConfig: [
             {
                 op: "CSV to String",
-                args: [','],
+                args: [","],
             },
         ],
     },
