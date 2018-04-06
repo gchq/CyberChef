cb66508b8a3cbd37b7765ba336adeb3e4bcb5e9e
n1474335 <n1474335@gmail.com>
2018-04-06 15:44:49 +0100

    jsesc is now transpiled with babel

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
+                exclude: /node_modules\/(?!jsesc)/,
                 loader: "babel-loader?compact=false"
             },
             {
