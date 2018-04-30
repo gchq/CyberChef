0dbc7d4fd19093b1a1e44664503c14e987db2189
n1474335 <n1474335@gmail.com>
2018-04-30 17:25:13 +0000

    Tidied up OperationError code.

--- src/core/errors/OperationError.mjs
+++ src/core/errors/OperationError.mjs
@@ -1,12 +1,16 @@
 /**
- * Create custom error type for handling operation input errors.
- * i.e. where the operation can handle the error and print a
- * message to the screen.
+ * Custom error type for handling operation input errors.
+ * i.e. where the operation can handle the error and print a message to the screen.
+ *
+ * @author d98762625 [d98762625@gmail.com]
+ * @copyright Crown Copyright 2018
+ * @license Apache-2.0
  */
 class OperationError extends Error {
     /**
      * Standard error constructor. Adds no new behaviour.
-     * @param args standard error args
+     *
+     * @param args - Standard error args
      */
     constructor(...args) {
         super(...args);
