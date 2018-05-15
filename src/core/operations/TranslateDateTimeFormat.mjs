03f435915bb083c59c4e019b7c34dd00a48b1c85
Matt C <matt@artemisbot.uk>
2018-05-15 18:50:04 +0100

    Imported OperationError to TranslateDateTimeFormat

--- src/core/operations/TranslateDateTimeFormat.mjs
+++ src/core/operations/TranslateDateTimeFormat.mjs
@@ -7,6 +7,7 @@
 import Operation from "../Operation";
 import moment from "moment-timezone";
 import {DATETIME_FORMATS, FORMAT_EXAMPLES} from "../lib/DateTime";
+import OperationError from "../errors/OperationError";
 
 /**
  * Translate DateTime Format operation
