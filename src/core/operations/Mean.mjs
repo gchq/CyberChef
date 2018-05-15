2716be397c1972e6c58a9170bc875496dd4f0c5a
d98762625 <d98762625@gmail.com>
2018-05-15 10:40:26 +0100

    port mean operation

---
+++ src/core/operations/Mean.mjs
@@ -0,0 +1,49 @@
+/**
+ * @author bwhitn [brian.m.whitney@outlook.com]
+ * @author d98762625 [d98762625@gmail.com]
+ * @copyright Crown Copyright 2018
+ * @license Apache-2.0
+ */
+
+import Operation from "../Operation";
+import Arithmetic from "../lib/Arithmetic";
+import BigNumber from "bignumber.js";
+
+/**
+ * Mean operation
+ */
+class Mean extends Operation {
+
+    /**
+     * Mean constructor
+     */
+    constructor() {
+        super();
+
+        this.name = "Mean";
+        this.module = "Default";
+        this.description = "Computes the mean (average) of a number list. If an item in the string is not a number it is excluded from the list.<br><br>e.g. <code>0x0a 8 .5 .5</code> becomes <code>4.75</code>";
+        this.inputType = "string";
+        this.outputType = "BigNumber";
+        this.args = [
+            {
+                "name": "Delimiter",
+                "type": "option",
+                "value": Arithmetic.DELIM_OPTIONS,
+            }
+        ];
+    }
+
+    /**
+     * @param {string} input
+     * @param {Object[]} args
+     * @returns {BigNumber}
+     */
+    run(input, args) {
+        const val = Arithmetic._mean(Arithmetic._createNumArray(input, args[0]));
+        return val instanceof BigNumber ? val : new BigNumber(NaN);
+    }
+
+}
+
+export default Mean;
