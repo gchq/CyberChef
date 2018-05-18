ec0ecf5151e1be500286ebaf53511d99af225ec0
d98762625 <d98762625@gmail.com>
2018-05-18 12:52:16 +0100

    add comments

--- src/core/lib/FlowControl.mjs
+++ src/core/lib/FlowControl.mjs
@@ -1,9 +1,18 @@
 /**
+ * Flow control functions
+ *
+ * @author d98762625 [d98762625@gmail.com]
+ * @copyright Crown Copyright 2018
+ * @license Apache-2.0
+ *
+ */
+
+/**
  * Returns the index of a label.
  *
  * @private
  * @param {Object} state
- * @param {string} name
+ * @param {string} name - The label name to look for.
  * @returns {number}
  */
 export function getLabelIndex(name, state) {
