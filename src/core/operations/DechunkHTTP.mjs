ed84614389e65d4755053a27588cbf984e720b3b
sevzero <russell.hole@gmail.com>
2018-05-24 13:39:14 +0000

    Fixed issues raised by Travis

--- src/core/operations/DechunkHTTP.mjs
+++ src/core/operations/DechunkHTTP.mjs
@@ -31,13 +31,13 @@ class DechunkHTTP extends Operation {
      * @returns {string}
      */
     run(input, args) {
-        let chunks = [];
+        const chunks = [];
         let chunkSizeEnd = input.indexOf("\n") + 1;
-        let lineEndings = input.charAt(chunkSizeEnd - 2) === "\r" ? "\r\n" : "\n";
-        let lineEndingsLength = lineEndings.length;
+        const lineEndings = input.charAt(chunkSizeEnd - 2) === "\r" ? "\r\n" : "\n";
+        const lineEndingsLength = lineEndings.length;
         let chunkSize = parseInt(input.slice(0, chunkSizeEnd), 16);
         while (!isNaN(chunkSize)) {
-            chunks.push(input.slice(chunkSizeEnd, chunkSize + chunkSizeEnd)); 
+            chunks.push(input.slice(chunkSizeEnd, chunkSize + chunkSizeEnd));
             input = input.slice(chunkSizeEnd + chunkSize + lineEndingsLength);
             chunkSizeEnd = input.indexOf(lineEndings) + lineEndingsLength;
             chunkSize = parseInt(input.slice(0, chunkSizeEnd), 16);
