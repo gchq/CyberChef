542809d5f54a24368b276bd4927fc91655288f75
GCHQ 77703 <gchq77703@gchq.gov.uk>
2018-08-26 21:11:38 +0100

    Add Moustache Template

---
+++ src/core/operations/RenderMoustache.mjs
@@ -0,0 +1,49 @@
+/**
+ * @author gchq77703 []
+ * @copyright Crown Copyright 2018
+ * @license Apache-2.0
+ */
+
+import Operation from "../Operation";
+import Moustache from "mustache";
+
+/**
+ * Render Moustache operation
+ */
+class RenderMoustache extends Operation {
+
+    /**
+     * Render Moustache constructor
+     */
+    constructor() {
+        super();
+
+        this.name = "Render Moustache";
+        this.module = "Default";
+        this.description = "Uses a provided Moustache template in order to render a JSON value.";
+        this.infoURL = "https://mustache.github.io/mustache.5.html";
+        this.inputType = "JSON";
+        this.outputType = "html";
+        this.args = [
+            {
+                name: "Template",
+                type: "text",
+                value: "Hello {{name}}."
+            },
+        ];
+    }
+
+    /**
+     * @param {JSON} input
+     * @param {Object[]} args
+     * @returns {html}
+     */
+    run(input, args) {
+        const [template] = args;
+
+        return Moustache.render(template, input)
+    }
+
+}
+
+export default RenderMoustache;
---
+++ test/tests/operations/RenderMoustache.mjs
@@ -0,0 +1,47 @@
+/**
+ * Render Moustache tests.
+ *
+ * @author gchq77703 []
+ *
+ * @copyright Crown Copyright 2018
+ * @license Apache-2.0
+ */
+import TestRegister from "../../TestRegister";
+
+
+TestRegister.addTests([
+    {
+        name: "Render Moustache",
+        input: `{
+    "name": "Dave",
+    "value": "£50,000",
+    "in_england": true,
+    "taxed_value": "£40,000"
+}`,
+        expectedOutput: `Hello Dave
+You have just won £50,000!
+Well, £40,000 after taxes.
+`,
+        recipeConfig: [
+            {
+                op: "Render Moustache",
+                args: [`Hello {{name}}
+You have just won {{value}}!
+{{#in_england}}
+Well, {{taxed_value}} after taxes.
+{{/in_england}}`]
+            }
+        ],
+    },
+    {
+        name: "Render Moustache",
+        input: `{}`,
+        expectedOutput: `Hello .`,
+        recipeConfig: [
+            {
+                op: "Render Moustache",
+                args: [`Hello {{name}}.`]
+            }
+        ],
+    },
+]);
