3abe99078e41a64889c3b6c0dd1fdbef436a21be
GCHQ 77703 <gchq77703@gchq.gov.uk>
2018-08-27 16:03:15 +0100

    Fix linting

--- test/tests/operations/LengthValueDecoder.mjs
+++ test/tests/operations/LengthValueDecoder.mjs
@@ -11,8 +11,8 @@ import TestRegister from "../../TestRegister";
 TestRegister.addTests([
     {
         name: "KeyValue",
-        input: [5,72,111,117,115,101,4,114,111,111,109,4,100,111,111,114],
-        expectedOutput: [{"key":[25],"length":5,"value":[72,111,117,115,101]},{"key":[73],"length":4,"value":[114,111,111,109]},{"key":[41],"length":4,"value":[100,111,111,114]}],
+        input: [5, 72, 111, 117, 115, 101, 4, 114, 111, 111, 109, 4, 100, 111, 111, 114],
+        expectedOutput: [{"key": [25], "length": 5, "value": [72, 111, 117, 115, 101]}, {"key": [73], "length": 4, "value": [114, 111, 111, 109]}, {"key": [41], "length": 4, "value": [100, 111, 111, 114]}],
         recipeConfig: [
             {
                 "op": "Length Value Decoder",
@@ -20,4 +20,4 @@ TestRegister.addTests([
             }
         ]
     },
-])
+]);
