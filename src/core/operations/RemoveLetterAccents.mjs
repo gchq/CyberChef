ac2466a304cf23d523c59b9da42b5f207176981a
Klaxon <klaxon@veyr.com>
2018-10-03 13:11:22 +1000

    create operation from npm run newop

---
+++ src/core/operations/RemoveLetterAccents.mjs
@@ -0,0 +1,56 @@
+/**
+ * @author Klaxon [klaxon@veyr.com]
+ * @copyright Crown Copyright 2018
+ * @license Apache-2.0
+ */
+
+import Operation from "../Operation";
+import OperationError from "../errors/OperationError";
+
+/**
+ * Remove Letter Accents operation
+ */
+class RemoveLetterAccents extends Operation {
+
+    /**
+     * RemoveLetterAccents constructor
+     */
+    constructor() {
+        super();
+
+        this.name = "Remove Letter Accents";
+        this.module = "Default";
+        this.description = "Replaces accented characters with their latin character equivalent.";
+        this.infoURL = "";
+        this.inputType = "string";
+        this.outputType = "string";
+        this.args = [
+            /* Example arguments. See the project wiki for full details.
+            {
+                name: "First arg",
+                type: "string",
+                value: "Don't Panic"
+            },
+            {
+                name: "Second arg",
+                type: "number",
+                value: 42
+            }
+            */
+        ];
+    }
+
+    /**
+     * @param {string} input
+     * @param {Object[]} args
+     * @returns {string}
+     */
+    run(input, args) {
+        // const [firstArg, secondArg] = args;
+
+        throw new OperationError("Test");
+    }
+
+}
+
+export default RemoveLetterAccents;
