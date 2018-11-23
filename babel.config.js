97e5e44723912dfbf60277547dda0f3990bdad3a
d98762625 <d98762625@gmail.com>
2018-11-23 10:54:16 +0000

    resolve conflicts with master

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = function(api) {
+    api.cache.forever();
+
+    return  {
+        "presets": [
+            ["@babel/preset-env", {
+                "targets": {
+                    "chrome": 40,
+                    "firefox": 35,
+                    "edge": 14,
+                    "safari": 11,
+                    "node": "6.5"
+                },
+                "modules": false,
+                "useBuiltIns": "entry"
+            }]
+        ],
+        "plugins": [
+            ["babel-plugin-transform-builtin-extend", {
+                "globals": ["Error"]
+            }]
+        ]
+    };
+};
