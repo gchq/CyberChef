3f7059a2352fc397ec035233f6958877687ceae3
Cynser <42423063+Cynser@users.noreply.github.com>
2018-12-12 17:49:11 +0000

    Remove unnecessary escape character

--- test/tests/operations/TextEncodingBruteForce.mjs
+++ test/tests/operations/TextEncodingBruteForce.mjs
@@ -12,7 +12,7 @@ TestRegister.addTests([
     {
         name: "Text Encoding Brute Force",
         input: "Р‘СѓР»РєС– РїСЂР°Р· Р»СЏРЅС–РІР° СЃР°Р±Р°РєСѓ.",
-        expectedMatch: /Windows\-1251 Cyrillic \(1251\): Булкі праз ляніва сабаку\./,
+        expectedMatch: /Windows-1251 Cyrillic \(1251\): Булкі праз ляніва сабаку\./,
         recipeConfig: [
             {
                 op: "Text Encoding Brute Force",
