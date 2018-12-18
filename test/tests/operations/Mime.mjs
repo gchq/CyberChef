fe6b6c8f472a2ac6bcebeeace546ecbd908c06a3
Brian Whitney <brian.m.whitney@outlook.com>
2018-12-18 18:42:22 -0500

    Modified Mime test

--- test/tests/operations/Mime.mjs
+++ test/tests/operations/Mime.mjs
@@ -10,9 +10,8 @@ import TestRegister from "../../TestRegister";
 TestRegister.addTests([
     {
         name: "Decode Mime Encoded Words",
-        input: "This is a GBK base64 encoded word: =?GBK?B?x/izx7ncvta52NPazfjC59Pfx+nQxc+i16g=?=.\nThis is a Cyrillic quoted word: =?utf-8?Q?=D0=A2=D0=B5=D1=81=D1=82_=D0=A2=D0=B5=D1=81=D1=82_=D0=A2=D0=B5=D1=81=D1=82?=.",
-        expectedOutput: ["This is a GBK base64 encoded word: 区城管局关于网络舆情信息专.",
-        "This is a Cyrillic quoted word: Тест Тест Тест."].join("\n"),
+        input: "This is a GBK base64 encoded word: =?GBK?B?572R57uc5Y6o5biI?=\nThis is a Cyrillic quoted word: =?utf-8?Q?=D0=A2=D0=B5=D1=81=D1=82_=D0=A2=D0=B5=D1=81=D1=82_=D0=A2=D0=B5=D1=81=D1=82?=.",
+        expectedOutput: "This is a GBK base64 encoded word: 网络厨师.\nThis is a Cyrillic quoted word: Кибер Шеф.",
         recipeConfig: [
             {
                 "op": "Decode Mime Encoded Words",
