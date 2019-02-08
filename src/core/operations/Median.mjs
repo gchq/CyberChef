d2b4c4035700698afd50f2759f2f24d9c22d39d2
n1474335 <n1474335@gmail.com>
2019-02-08 16:54:04 +0000

    Fixed BigNumber/XRegExp incompatibility. CLoses #481

--- src/core/operations/Median.mjs
+++ src/core/operations/Median.mjs
@@ -43,7 +43,7 @@ class Median extends Operation {
      */
     run(input, args) {
         const val = median(createNumArray(input, args[0]));
-        return val instanceof BigNumber ? val : new BigNumber(NaN);
+        return BigNumber.isBigNumber(val) ? val : new BigNumber(NaN);
     }
 
 }
