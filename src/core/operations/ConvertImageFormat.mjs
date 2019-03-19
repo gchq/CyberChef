b312e179047d3f6bd374226eec975248e5833f41
j433866 <j433866@gmail.com>
2019-03-19 13:54:39 +0000

    Change title to title case

--- src/core/operations/ConvertImageFormat.mjs
+++ src/core/operations/ConvertImageFormat.mjs
@@ -21,7 +21,7 @@ class ConvertImageFormat extends Operation {
     constructor() {
         super();
 
-        this.name = "Convert image format";
+        this.name = "Convert Image Format";
         this.module = "Image";
         this.description = "Converts an image between different formats. Supported formats:<br><ul><li>Joint Photographic Experts Group (JPEG)</li><li>Portable Network Graphics (PNG)</li><li>Bitmap (BMP)</li><li>Tagged Image File Format (TIFF)</li></ul><br>Note: GIF files are supported for input, but cannot be outputted.";
         this.infoURL = "https://wikipedia.org/wiki/Image_file_formats";
