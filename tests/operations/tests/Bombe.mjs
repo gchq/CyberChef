4add484d2ad7eb88779525d1a68f942e5b73d910
d98762625 <d98762625@gmail.com>
2019-03-20 12:00:22 +0000

    update TestRegister relative path in some tests

--- tests/operations/tests/Bombe.mjs
+++ tests/operations/tests/Bombe.mjs
@@ -4,7 +4,7 @@
  * @copyright Crown Copyright 2019
  * @license Apache-2.0
  */
-import TestRegister from "../TestRegister";
+import TestRegister from "../../lib/TestRegister";
 
 TestRegister.addTests([
     {
