c92e09e6eb6dd19ba6ac5993f9caf8329603fa2e
Tan Zhen Yong <Xenonym@users.noreply.github.com>
2019-10-07 17:44:00 +0800

    Add Argon2 hash compare operation

--- tests/node/tests/operations.mjs
+++ tests/node/tests/operations.mjs
@@ -138,6 +138,13 @@ Tiger-128`;
         assert.strictEqual(result.toString(), "$argon2i$v=19$m=4096,t=3,p=1$c29tZXNhbHQ$s43my9eBljQADuF/LWCG8vGqwAJzOorKQ0Yog8jFvbw");
     }),
 
+    it("argon2 compare", async () => {
+        const result = await chef.argon2Compare("argon2password", {
+            hash: "$argon2i$v=19$m=4096,t=3,p=1$c29tZXNhbHQ$s43my9eBljQADuF/LWCG8vGqwAJzOorKQ0Yog8jFvbw"
+        });
+        assert.strictEqual(result.toString(), "Match: argon2password");
+    }),
+
     it("Bcrypt", async () => {
         const result = await chef.bcrypt("Put a Sock In It");
         const strResult = result.toString();
