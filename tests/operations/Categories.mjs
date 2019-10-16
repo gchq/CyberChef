67d0bdaec40bb27c3c1892e61d20bfb60093d542
n1474335 <n1474335@gmail.com>
2019-10-16 15:10:03 +0100

    Added test to ensure all operations are in a category. Added various operations to categories.

---
+++ tests/operations/Categories.mjs
@@ -0,0 +1,19 @@
+import TestRegister from "../lib/TestRegister.mjs";
+import Categories from "../../src/core/config/Categories.json";
+import OperationConfig from "../../src/core/config/OperationConfig.json";
+import it from "../node/assertionHandler.mjs";
+import assert from "assert";
+
+TestRegister.addApiTests([
+    it("Categories: operations should be in a category", () => {
+        const catOps = [];
+        Categories.forEach(cat => {
+            catOps.push(...cat.ops);
+        });
+
+        for (const op in OperationConfig) {
+            assert(catOps.includes(op), `'${op}' operation is not present in any category`);
+        }
+    }),
+
+]);
