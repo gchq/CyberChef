628f2592f761e4c0f556d906c230efbc342990b7
Macide Celik <macide.35cc@gmail.com>
2019-10-27 18:49:47 +0300

    Delete postcss.config.js

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    plugins: [
-        require("postcss-import"),
-        require("autoprefixer"),
-        require("postcss-css-variables")({
-            preserve: true
-        }),
-    ]
-};
