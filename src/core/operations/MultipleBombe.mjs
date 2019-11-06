9ed2b26933ed7645ec7f64d8eb665e514342de7b
n1474335 <n1474335@gmail.com>
2019-11-06 12:14:22 +0000

    Tidied up Lorenz operation and created new Bletchley module for WW2-era ciphers

--- src/core/operations/MultipleBombe.mjs
+++ src/core/operations/MultipleBombe.mjs
@@ -54,7 +54,7 @@ class MultipleBombe extends Operation {
         super();
 
         this.name = "Multiple Bombe";
-        this.module = "Default";
+        this.module = "Bletchley";
         this.description = "Emulation of the Bombe machine used to attack Enigma. This version carries out multiple Bombe runs to handle unknown rotor configurations.<br><br>You should test your menu on the single Bombe operation before running it here. See the description of the Bombe operation for instructions on choosing a crib.<br><br>More detailed descriptions of the Enigma, Typex and Bombe operations <a href='https://github.com/gchq/CyberChef/wiki/Enigma,-the-Bombe,-and-Typex'>can be found here</a>.";
         this.infoURL = "https://wikipedia.org/wiki/Bombe";
         this.inputType = "string";
