3bb42a64024394f7fa4c7f9c41b8b0ae5157ae6e
n1073645 <n1073645@gmail.com>
2019-12-17 12:15:11 +0000

    Gzip tests added

--- tests/operations/index.mjs
+++ tests/operations/index.mjs
@@ -41,6 +41,7 @@ import "./tests/DateTime.mjs";
 import "./tests/ExtractEmailAddresses.mjs";
 import "./tests/Fork.mjs";
 import "./tests/FromDecimal.mjs";
+import "./tests/Gzip.mjs";
 import "./tests/Hash.mjs";
 import "./tests/HaversineDistance.mjs";
 import "./tests/Hexdump.mjs";
