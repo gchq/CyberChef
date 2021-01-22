345ad741b376258ce8ad44c86693c004f0396f10
n1474335 <n1474335@gmail.com>
2021-01-22 14:47:33 +0000

    Fixed sed command in postinstall script to be platform dependent

---
+++ postinstall.sh
@@ -0,0 +1,6 @@
+# Add file extensions to Crypto-Api imports
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  find ./node_modules/crypto-api/src/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i '' -e '/\.mjs/!s/\(from "\.[^"]*\)";/\1.mjs";/g'
+else
+  find ./node_modules/crypto-api/src/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i -e '/\.mjs/!s/\(from "\.[^"]*\)";/\1.mjs";/g'
+fi
