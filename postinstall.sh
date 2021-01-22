e16ce1d9c2c6c69bcb267fdb8ac08ec9203e7cc6
n1474335 <n1474335@gmail.com>
2021-01-22 14:53:53 +0000

    Specify node version

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Add file extensions to Crypto-Api imports
 if [[ "$OSTYPE" == "darwin"* ]]; then
   find ./node_modules/crypto-api/src/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i '' -e '/\.mjs/!s/\(from "\.[^"]*\)";/\1.mjs";/g'
