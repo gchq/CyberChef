9c462418e946ab39895f6ec259c9a39d04f24333
Kaustubh BM <kaustubhbm3574@gmail.com>
2021-05-13 21:59:09 +0530

    Update Utils.mjs
    Just included the hyphen ( - ) in the charRep function to enable it to be used in the Multi Tap Cipher Operation.

--- src/core/Utils.mjs
+++ src/core/Utils.mjs
@@ -1169,6 +1169,7 @@ class Utils {
             "Comma":         ",",
             "Semi-colon":    ";",
             "Colon":         ":",
+            "Hyphen":        "-",
             "Tab":           "\t",
             "Line feed":     "\n",
             "CRLF":          "\r\n",
