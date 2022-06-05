1ab444bda2d40e87dd0db7a9d156556bd8bb965b
n1073645 <n1073645@gmail.com>
2022-06-05 18:40:46 +0100

    Fix tabs in tests file.

--- tests/operations/tests/ELFInfo.mjs
+++ tests/operations/tests/ELFInfo.mjs
@@ -3,18 +3,18 @@
  * @copyright Crown Copyright 2022
  * @license Apache-2.0
  */
- import TestRegister from "../../lib/TestRegister.mjs";
+import TestRegister from "../../lib/TestRegister.mjs";
 
- TestRegister.addTests([
-     {
-         name: "ELF Info invalid ELF.",
-         input: "\x7f\x00\x00\x00",
-         expectedOutput: "Invalid ELF",
-         recipeConfig: [
-             {
-                 op: "ELF Info",
-                 args: [],
-             },
-         ],
-     },
- ]);
+TestRegister.addTests([
+    {
+        name: "ELF Info invalid ELF.",
+        input: "\x7f\x00\x00\x00",
+        expectedOutput: "Invalid ELF",
+        recipeConfig: [
+            {
+                op: "ELF Info",
+                args: [],
+            },
+        ],
+    },
+]);
