4274e8f3a21ed10e0e91815802412fa8c061df51
n1474335 <n1474335@gmail.com>
2022-06-06 15:20:31 +0100

    Fixed PEM wiki link

--- src/core/operations/PEMToHex.mjs
+++ src/core/operations/PEMToHex.mjs
@@ -24,7 +24,7 @@ class PEMToHex extends Operation {
         this.name = "PEM to Hex";
         this.module = "Default";
         this.description = "Converts PEM (Privacy Enhanced Mail) format to a hexadecimal DER (Distinguished Encoding Rules) string.";
-        this.infoURL = "https://en.wikipedia.org/wiki/Privacy-Enhanced_Mail#Format";
+        this.infoURL = "https://wikipedia.org/wiki/Privacy-Enhanced_Mail#Format";
         this.inputType = "string";
         this.outputType = "string";
         this.args = [];
