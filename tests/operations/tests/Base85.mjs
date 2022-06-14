00d754d46695d0f8278e093dc63b75c126de7831
John L <john19696@protonmail.com>
2022-06-14 15:57:04 +0100

    lint fixes

--- tests/operations/tests/Base85.mjs
+++ tests/operations/tests/Base85.mjs
@@ -7,8 +7,8 @@
  */
 import TestRegister from "../../lib/TestRegister.mjs";
 
-// Example from Wikipedia 
-const wpExample = "Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure."
+// Example from Wikipedia
+const wpExample = "Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.";
 // Escape newline, quote & backslash
 const wpOutput = "9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,O<\
 DJ+*.@<*K0@<6L(Df-\\0Ec5e;DffZ(EZee.Bl.9pF\"AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKYi(\
