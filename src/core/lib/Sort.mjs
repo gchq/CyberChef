cb023089bbadd609ae9daefd426bdb86ea735887
Didier Stevens <didier.stevens@gmail.com>
2022-10-30 15:33:11 +0100

    Operation Sort: added value Length to option Order

--- src/core/lib/Sort.mjs
+++ src/core/lib/Sort.mjs
@@ -103,3 +103,15 @@ export function hexadecimalSort(a, b) {
 
     return a.localeCompare(b);
 }
+
+/**
+ * Comparison operation for sorting of lines by length
+ *
+ * @param {string} a
+ * @param {string} b
+ * @returns {number}
+ */
+export function lengthSort(a, b) {
+    return a.length - b.length;
+}
+
