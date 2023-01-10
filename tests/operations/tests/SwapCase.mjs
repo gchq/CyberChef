d5b72548fc81f5e5b60c094b4bdd6dddae8b2c2a
MikeCAT <mikecat@users.noreply.github.com>
2023-01-11 05:48:05 +0900

    add new operation: Swap case

---
+++ tests/operations/tests/SwapCase.mjs
@@ -0,0 +1,33 @@
+/**
+ * @author mikecat
+ * @copyright Crown Copyright 2023
+ * @license Apache-2.0
+ */
+import TestRegister from "../../lib/TestRegister.mjs";
+
+TestRegister.addTests([
+    {
+        "name": "Swap Case: basic example",
+        "input": "Hello, World!",
+        "expectedOutput": "hELLO, wORLD!",
+        "recipeConfig": [
+            {
+                "op": "Swap case",
+                "args": [
+                ],
+            },
+        ],
+    },
+    {
+        "name": "Swap Case: empty input",
+        "input": "",
+        "expectedOutput": "",
+        "recipeConfig": [
+            {
+                "op": "Swap case",
+                "args": [
+                ],
+            },
+        ],
+    },
+]);
