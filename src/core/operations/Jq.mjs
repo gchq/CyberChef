d2da11c79a59412b3ae480a5c2e05ff4421bd113
zhzy0077 <zhzy0077@hotmail.com>
2023-07-25 14:42:43 +0800

    Support jq.

---
+++ src/core/operations/Jq.mjs
@@ -0,0 +1,57 @@
+/**
+ * @author zhzy0077 [zhzy0077@hotmail.com]
+ * @copyright Crown Copyright 2023
+ * @license Apache-2.0
+ */
+
+import Operation from "../Operation.mjs";
+import OperationError from "../errors/OperationError.mjs";
+import jq from "jq-web";
+
+/**
+ * jq operation
+ */
+class Jq extends Operation {
+
+    /**
+     * Jq constructor
+     */
+    constructor() {
+        super();
+
+        this.name = "jq";
+        this.module = "Code";
+        this.description = "jq is a lightweight and flexible command-line JSON processor.";
+        this.infoURL = "https://github.com/jqlang/jq";
+        this.inputType = "JSON";
+        this.outputType = "string";
+        this.args = [
+            {
+                name: "Query",
+                type: "string",
+                value: ""
+            }
+        ];
+    }
+
+    /**
+     * @param {JSON} input
+     * @param {Object[]} args
+     * @returns {string}
+     */
+    run(input, args) {
+        const [query] = args;
+        let result;
+
+        try {
+            result = jq.json(input, query);
+        } catch (err) {
+            throw new OperationError(`Invalid jq expression: ${err.message}`);
+        }
+
+        return JSON.stringify(result);
+    }
+
+}
+
+export default Jq;
