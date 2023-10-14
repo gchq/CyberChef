e9160f3de09885f4dddd080473f291b71eed1910
CPlusSharp <cgraefe83@gmail.com>
2023-10-15 00:06:46 +0200

    Public Key from Private Key

--- tests/operations/index.mjs
+++ tests/operations/index.mjs
@@ -75,6 +75,7 @@ import "./tests/ParseQRCode.mjs";
 import "./tests/PEMtoHex.mjs";
 import "./tests/PowerSet.mjs";
 import "./tests/PubKeyFromCert.mjs";
+import "./tests/PubKeyFromPrivKey.mjs";
 import "./tests/Regex.mjs";
 import "./tests/Register.mjs";
 import "./tests/Rotate.mjs";
