28e2a391b88b04bc8cfcf768957d4b9c27f0a288
CPlusSharp <cgraefe83@gmail.com>
2023-10-15 00:06:46 +0200

    Public Key from Private Key

--- tests/operations/index.mjs
+++ tests/operations/index.mjs
@@ -121,6 +121,7 @@ import "./tests/PHP.mjs";
 import "./tests/PowerSet.mjs";
 import "./tests/Protobuf.mjs";
 import "./tests/PubKeyFromCert.mjs";
+import "./tests/PubKeyFromPrivKey.mjs";
 import "./tests/Rabbit.mjs";
 import "./tests/RAKE.mjs";
 import "./tests/Regex.mjs";
