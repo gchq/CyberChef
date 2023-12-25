77042abc2348fcb19e4f25d89d356f4d2697382e
Maxime THIEBAUT <46688461+0xThiebaut@users.noreply.github.com>
2023-12-25 01:17:16 +0100

    Add support for LZNT1 decompression.

--- tests/operations/index.mjs
+++ tests/operations/index.mjs
@@ -62,6 +62,7 @@ import "./tests/JSONtoCSV.mjs";
 import "./tests/JWTDecode.mjs";
 import "./tests/JWTSign.mjs";
 import "./tests/JWTVerify.mjs";
+import "./tests/LZNT1Decompress.mjs";
 import "./tests/MS.mjs";
 import "./tests/Magic.mjs";
 import "./tests/MorseCode.mjs";
