0fa8be7dd07be28f60ae2bc4851e73a5d1a69c1c
j264415 <j264415@protonmail.com>
2024-03-07 09:34:14 +0000

    fixed lintting issue

--- src/web/waiters/ControlsWaiter.mjs
+++ src/web/waiters/ControlsWaiter.mjs
@@ -362,7 +362,6 @@ class ControlsWaiter {
 
         for (let i = 0; i < tabs.length; i++) {
             tabs[i].addEventListener("keydown", this.changeTabs, false);
-            // tabs[i].addEventListener("keydown", this.navigateFAQList, false);
         }
 
         const reportBugInfo = document.getElementById("report-bug-info");
@@ -406,36 +405,36 @@ ${navigator.userAgent}
             }
         } else if (ev.key === "Tab" && !ev.shiftKey && ev.target === document.getElementById("tab-1")) {
             document.getElementById("faqs").querySelector("[class='btn btn-primary']").focus();
-        }  else if (ev.key === "Tab" && !ev.shiftKey && ev.target === document.getElementById("tab-2")) {
+        } else if (ev.key === "Tab" && !ev.shiftKey && ev.target === document.getElementById("tab-2")) {
             document.getElementById("report-bug").querySelector("[class='btn btn-primary']").focus();
-        }  else if (ev.key === "Tab" && !ev.shiftKey && ev.target === document.getElementById("tab-3")) {
+        } else if (ev.key === "Tab" && !ev.shiftKey && ev.target === document.getElementById("tab-3")) {
             document.getElementById("about").querySelector("[href]").focus();
-        }  else if (ev.key === "Tab" && !ev.shiftKey && ev.target === document.getElementById("tab-4")) {
+        } else if (ev.key === "Tab" && !ev.shiftKey && ev.target === document.getElementById("tab-4")) {
             const button = document.getElementById("support-modal").getElementsByClassName("modal-footer");
             const close = button[0].firstElementChild;
             close.focus();
-        } else if (ev.key === "Enter" || ev.key === "Space" ||ev.key === " ") {
+        } else if (ev.key === "Enter" || ev.key === "Space" || ev.key === " ") {
             tab.click();
-        }  
+        }
     }
 
-     /**
-    * @param {Event} ev
-    */
+    /**
+   * @param {Event} ev
+   */
     navigateFAQList(ev) {
-       
+
         const el = ev.target.nextElementSibling;
-        if (ev.key === "Enter" || ev.key === "Space"|| ev.key === " "){
+        if (ev.key === "Enter" || ev.key === "Space" || ev.key === " ") {
             ev.preventDefault();
             const question = el.classList;
-            if(question !== undefined && question.value){
+            if (question !== undefined && question.value) {
                 if (!question.value.includes("show")) {
                     question.add("show");
                 } else if (question.contains("show")) {
                     question.remove("show");
                 }
             }
-        } 
+        }
     }
 
     /**
