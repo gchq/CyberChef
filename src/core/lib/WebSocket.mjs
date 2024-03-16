7c813e0be95991554fc0ddd0fc7a11f65a22861c
cod <sysenter.dev@gmail.com>
2024-03-16 17:44:53 +0300

    Create WebSocket.mjs

---
+++ src/core/lib/WebSocket.mjs
@@ -0,0 +1,9 @@
+/**
+ * WebSocket operation resources.
+ *
+ * @author cod [sysenter.dev@gmail.com]
+ * @copyright Crown Copyright 2024
+ * @license Apache-2.0
+ */
+
+export const WEBSOCKET_OPCODE = ["continuation frame", "text frame", "binary frame", "rsv3", "rsv4", "rsv5", "rsv6", "rsv7", "ping", "pong", "rsvb", "rsvc", "rsvd", "rsve", "rsvf"];
