077b11e33bc9e1015147380309cd964f52bd657c
mshwed <matthew.shwed@gmail.com>
2024-04-01 22:30:18 -0400

    Fixed op name in test

--- tests/operations/tests/ExtractHashes.mjs
+++ tests/operations/tests/ExtractHashes.mjs
@@ -14,7 +14,7 @@ TestRegister.addTests([
         expectedOutput: "9e107d9d372bb6826bd81d3542a419d6",
         recipeConfig: [
             {
-                "op": "Extract Hashes",
+                "op": "Extract hashes",
                 "args": [32, false, false]
             },
         ],
@@ -25,7 +25,7 @@ TestRegister.addTests([
         expectedOutput: "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12",
         recipeConfig: [
             {
-                "op": "Extract Hashes",
+                "op": "Extract hashes",
                 "args": [40, false, false]
             },
         ],
@@ -36,7 +36,7 @@ TestRegister.addTests([
         expectedOutput: "d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592",
         recipeConfig: [
             {
-                "op": "Extract Hashes",
+                "op": "Extract hashes",
                 "args": [64, false, false]
             },
         ],
@@ -47,7 +47,7 @@ TestRegister.addTests([
         expectedOutput: "07e547d9586f6a73f73fbac0435ed76951218fb7d0c8d788a309d785436bbb642e93a252a954f23912547d1e8a3b5ed6e1bfd7097821233fa0538f3db854fee6",
         recipeConfig: [
             {
-                "op": "Extract Hashes",
+                "op": "Extract hashes",
                 "args": [128, false, false]
             },
         ],
@@ -58,7 +58,7 @@ TestRegister.addTests([
         expectedOutput: "9e107d9d372bb6826bd81d3542a419d6\n2fd4e1c67a2d28fced849ee1bb76e7391b93eb12\nd7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592",
         recipeConfig: [
             {
-                "op": "Extract Hashes",
+                "op": "Extract hashes",
                 "args": [0, true, false]
             },
         ],
@@ -69,7 +69,7 @@ TestRegister.addTests([
         expectedOutput: "Total Results: 3\n\n9e107d9d372bb6826bd81d3542a419d6\n2fd4e1c67a2d28fced849ee1bb76e7391b93eb12\nd7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592",
         recipeConfig: [
             {
-                "op": "Extract Hashes",
+                "op": "Extract hashes",
                 "args": [0, true, true]
             },
         ],
