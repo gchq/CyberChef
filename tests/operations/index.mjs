7b2d572902c962cd00549ea758d05125b97a224c
n1474335 <n1474335@gmail.com>
2024-04-12 14:41:00 +0100

    Added 'JA4Server Fingerprint' operation

--- tests/operations/index.mjs
+++ tests/operations/index.mjs
@@ -83,7 +83,7 @@ import "./tests/HKDF.mjs";
 import "./tests/Image.mjs";
 import "./tests/IndexOfCoincidence.mjs";
 import "./tests/JA3Fingerprint.mjs";
-import "./tests/JA4Fingerprint.mjs";
+import "./tests/JA4.mjs";
 import "./tests/JA3SFingerprint.mjs";
 import "./tests/JSONBeautify.mjs";
 import "./tests/JSONMinify.mjs";
