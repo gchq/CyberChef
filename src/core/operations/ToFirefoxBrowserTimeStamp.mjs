7c75618d34359c1434872a2bec1aae7fafaf55e2
Michael H <mykulh@users.noreply.github.com>
2024-06-12 09:40:55 +0100

    Update ToFirefoxBrowserTimeStamp.mjs

--- src/core/operations/ToFirefoxBrowserTimeStamp.mjs
+++ src/core/operations/ToFirefoxBrowserTimeStamp.mjs
@@ -43,8 +43,8 @@ class ToFirefoxBrowserTimestamp extends Operation {
      * @throws {OperationError} if invalid unit
      */
     run(input, args) {
-        const [showDateTime] = args, 
-	d = moment.utc(input);
+        const [showDateTime] = args,
+    	d = moment.utc(input);
         let result = (d.unix() * 1000000);
         return showDateTime ? `${result} (${d.tz("UTC").format("ddd D MMMM YYYY HH:mm:ss")} UTC)` : result.toString();
     }
