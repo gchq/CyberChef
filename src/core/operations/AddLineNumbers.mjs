3dddb516c9470d609d87318ffe0469c70802022c
Adamkadaban <adamkadaban@gmail.com>
2024-07-27 20:06:18 -0700

    add offset field to 'Add Line Numbers' operation

--- src/core/operations/AddLineNumbers.mjs
+++ src/core/operations/AddLineNumbers.mjs
@@ -22,7 +22,13 @@ class AddLineNumbers extends Operation {
         this.description = "Adds line numbers to the output.";
         this.inputType = "string";
         this.outputType = "string";
-        this.args = [];
+        this.args = [
+            {
+                "name": "Offset",
+                "type": "number",
+                "value": 0
+            }
+        ];
     }
 
     /**
@@ -33,10 +39,11 @@ class AddLineNumbers extends Operation {
     run(input, args) {
         const lines = input.split("\n"),
             width = lines.length.toString().length;
+        const offset = parseInt(args[0], 10);
         let output = "";
 
         for (let n = 0; n < lines.length; n++) {
-            output += (n+1).toString().padStart(width, " ") + " " + lines[n] + "\n";
+            output += (n+1+offset).toString().padStart(width, " ") + " " + lines[n] + "\n";
         }
         return output.slice(0, output.length-1);
     }
