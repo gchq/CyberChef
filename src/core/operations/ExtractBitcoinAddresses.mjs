8316056667e1a42b4248eb2bf8f870a935bc9d8a
Jonathan Homer <homer.jonathan@gmail.com>
2025-02-16 21:44:27 +0000

    Minor fix to capture arg of display totals

--- src/core/operations/ExtractBitcoinAddresses.mjs
+++ src/core/operations/ExtractBitcoinAddresses.mjs
@@ -38,7 +38,7 @@ class ExtractBitcoinAddresses extends Operation {
      * @returns {string}
      */
     run(input, args) {
-        const [displayTotal] = args,
+        const displayTotal = args[0],
             bitcoin = "(?:[13]{1}[a-km-zA-HJ-NP-Z1-9]{26,33}|bc1[a-z0-9]{39,59})";
 
         const bitcoins  = bitcoin;
