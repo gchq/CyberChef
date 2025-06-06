3cd45c5f68d7b1ed987ee4941e10dcc6710a8f33
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-06-06 12:36:22 +0100

    Create constructor

--- src/core/operations/LineBreak.mjs
+++ src/core/operations/LineBreak.mjs
@@ -10,6 +10,32 @@ import Operation from "../Operation.mjs";
 /**
  * Line Break operation
  */
-class LineBreak extends Operation {}
+class LineBreak extends Operation {
+
+    /**
+     * LineBreak constructor
+     */
+    constructor() {
+        super();
+
+        this.name = "Line Break";
+        this.module = "Default";
+        this.description = "Breaks the input text every <code>n</code> characters.";
+        this.inputType = "ArrayBuffer";
+        this.outputType = "string";
+        this.args = [
+            {
+                "name": "Line break width",
+                "type": "number",
+                "value": 16,
+                "min": 1
+            }, {
+                "name": "Remove leading whitespace",
+                "type": "boolean",
+                "value": false
+            }
+        ];
+    }
+}
 
 export default LineBreak;
