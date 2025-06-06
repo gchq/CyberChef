1ca9f23e4775f6386942bfda53af7c13f9c367e7
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-06-06 12:36:05 +0100

    Create line break operation

---
+++ src/core/operations/LineBreak.mjs
@@ -0,0 +1,15 @@
+/**
+ * @author ThomasNotTom
+ * @copyright Crown Copyright 2016
+ * @license Apache-2.0
+ */
+
+import Operation from "../Operation.mjs";
+
+
+/**
+ * Line Break operation
+ */
+class LineBreak extends Operation {}
+
+export default LineBreak;
