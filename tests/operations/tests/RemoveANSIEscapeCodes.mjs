53adbca9be971334f2b055acd1ff5cba508bd3f8
Louis-Ladd <lewisharshman1@gmail.com>
2026-01-04 17:51:48 -0700

    Add tests for remove ANSI escape codes operation

--- tests/operations/tests/RemoveANSIEscapeCodes.mjs
+++ tests/operations/tests/RemoveANSIEscapeCodes.mjs
@@ -3,4 +3,43 @@
  * @copyright Crown Copyright 2025
  * @license Apache-2.0
  */
+import TestRegister from "../../lib/TestRegister.mjs";
 
+TestRegister.addTests([
+    {
+        "name": "Remove ANSI Escape Codes: x1b escape code",
+        "input": "\\x1b[31;1;3;4;9mHello, \\x1b[31;1;3;4;9mWorld!",
+        "expectedOutput": "Hello, World!",
+        "recipeConfig": [
+            {
+                "op": "Remove ANSI Escape Codes",
+                "args": [
+                ],
+            },
+        ],
+    },
+    {
+        "name": "Remove ANSI Escape Codes: 033 escape code",
+        "input": "\\033[32;1;3;4;9mHello, \\033[32;1;3;4;9mWorld!",
+        "expectedOutput": "Hello, World!",
+        "recipeConfig": [
+            {
+                "op": "Remove ANSI Escape Codes",
+                "args": [
+                ],
+            },
+        ],
+    },
+    {
+        "name": "Remove ANSI Escape Codes: e escape code",
+        "input": "\\e[32;1;3;4;9mHello, \\e[32;1;3;4;9mWorld!",
+        "expectedOutput": "Hello, World!",
+        "recipeConfig": [
+            {
+                "op": "Remove ANSI Escape Codes",
+                "args": [
+                ],
+            },
+        ],
+    }
+]);
