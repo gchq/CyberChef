b9226ac6c8a6c1e132b5d893a7864d997b37793f
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-09-12 00:57:15 +0100

    Add proper noun case operation

---
+++ src/core/operations/ToProperNounCase.mjs
@@ -0,0 +1,40 @@
+/**
+ * @copyright Crown Copyright 2026
+ * @license Apache-2.0
+ */
+
+import Operation from "../Operation.mjs";
+
+/**
+ * To Proper Noun Case operation
+ */
+class ToProperNounCase extends Operation {
+
+    /**
+     * ToProperNounCase constructor
+     */
+    constructor() {
+        super();
+
+        this.name = "To Proper Noun Case";
+        this.module = "Default";
+        this.description = "Converts the first letter of each word to upper case and the remaining letters to lower case, while preserving punctuation and spacing.";
+        this.inputType = "string";
+        this.outputType = "string";
+        this.args = [];
+    }
+
+    /**
+     * @param {string} input
+     * @returns {string}
+     */
+    run(input) {
+        return input.toLowerCase().replace(
+            /(^|[^\p{L}\p{N}])(\p{L})/gu,
+            (match, prefix, letter) => prefix + letter.toUpperCase()
+        );
+    }
+
+}
+
+export default ToProperNounCase;
